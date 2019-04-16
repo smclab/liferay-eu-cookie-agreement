7e9341cf7c8080084d48e302ce4ed0689b532cac
samuele.donato <samuele.donato@smc.it>
2019-04-16 11:45:45 +0200

    Correct the following errors:  - There are sources not part of the project.  - Copyright (module is distributed as LGPL)  - ensure files use Unix EOL (no CR-LF)

--- modules/privacy-web/src/main/java/it/smc/liferay/privacy/web/portlet/PrivacyAdminPortlet.java
+++ modules/privacy-web/src/main/java/it/smc/liferay/privacy/web/portlet/PrivacyAdminPortlet.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2013 SMC Treviso Srl. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package it.smc.liferay.privacy.web.portlet;
 
 import java.io.IOException;
@@ -23,6 +37,9 @@ import com.liferay.portal.kernel.util.WebKeys;
 import it.smc.liferay.privacy.web.util.PrivacyPortletKeys;
 import it.smc.liferay.privacy.web.util.PrivacyUtil;
 
+/**
+ * @author SMC Treviso
+ */
 @Component(
 	immediate = true,
 	property = {
--- modules/privacy-web/src/main/java/it/smc/liferay/privacy/web/util/PrivacyUtil.java
+++ modules/privacy-web/src/main/java/it/smc/liferay/privacy/web/util/PrivacyUtil.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2013 SMC Treviso Srl. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package it.smc.liferay.privacy.web.util;
 
 import javax.portlet.PortletPreferences;
@@ -13,6 +27,9 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.Validator;
 
+/**
+ * @author SMC Treviso
+ */
 public class PrivacyUtil {
 
 	public static final String PRIVACY_READ = "PRIVACY_READ";
